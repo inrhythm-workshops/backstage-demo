b42ab4e46e9d11626c4e7f42706ffb8aa28e55cc
tashrafy <tashrafy91@gmail.com>
2023-03-29 09:52:40 -0400

    chore(update): adding config for cra template

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,5 +1,8 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { ScmIntegrations } from '@backstage/integration';
+import { createRouter, createBuiltinActions } from '@backstage/plugin-scaffolder-backend';
+import { createHttpBackstageAction } from '@roadiehq/scaffolder-backend-module-http-request';
+import { createAwsS3CpAction } from '@roadiehq/scaffolder-backend-module-aws';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
 
@@ -10,12 +13,26 @@ export default async function createPlugin(
     discoveryApi: env.discovery,
   });
 
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+  const actions = [
+    createHttpBackstageAction({ discovery: env.discovery }),
+    createAwsS3CpAction(),
+    ...createBuiltinActions({
+      integrations,
+      config: env.config,
+      catalogClient,
+      reader: env.reader,
+    }),
+  ];
+
   return await createRouter({
     logger: env.logger,
     config: env.config,
     database: env.database,
     reader: env.reader,
     catalogClient,
+    actions,
     identity: env.identity,
   });
 }
